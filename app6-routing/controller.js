d54fc8d67014fcbe81220007107ec84f8ee50f42
adamShaleen <adamshaleen@gmail.com>
2016-04-12 20:23:13 -0600

    finished lesson 6 for the first time

---
+++ app6-routing/controller.js
@@ -0,0 +1,8 @@
+angular.module('routingPractice').controller('controller', function($scope, service) {
+
+$scope.view1 = service.view1;
+$scope.view2 = service.view2;
+$scope.view3 = service.view3;
+
+
+});
---
+++ app6-routing/service.js
@@ -0,0 +1,8 @@
+angular.module('routingPractice').service('service', function() {
+
+this.view1 = 'this is view1';
+this.view2 = 'this is view2';
+this.view3 = 'this is view3';
+
+
+});

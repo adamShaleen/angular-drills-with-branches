3c236b0e86c7a7cc41d78e1a133ae60f5a0967bc
adamShaleen <adamshaleen@gmail.com>
2016-04-16 18:35:07 -0600

    finished attempt 10

--- app1-basics/controller.js
+++ app1-basics/controller.js
@@ -2,12 +2,13 @@ angular.module('attempt10').controller('controller', function($scope, service) {
 
 $scope.displayFriends = service.getFriends();
 
-$scope.displayStarWarsData = function() {
-    $scope.data = service.getStarWarsData;
-    console.log(data.response);
-        return data.response;
+$scope.displayData = function() {
+    service.getStarWarsData().then(function(response) {
+        $scope.data = response;
+    });
 };
 
-$scope.displayStarWarsData();
+
+$scope.displayData();
 
 });
